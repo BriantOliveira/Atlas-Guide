9d1e4737888ecaa013266c977447e7d414c42c94
Briant Oliveira <briantmoliveira@gmail.com>
2017-12-04 15:18:31 -0800

    Itinerary association

--- models/itinerary.js
+++ models/itinerary.js
@@ -5,12 +5,12 @@ module.exports = (sequelize, DataTypes) => {
     user_id: DataTypes.INTEGER,
     venue_id: DataTypes.INTEGER,
     user_phone_number: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
-  });
+});
+
+Itinerary.associate = function (models) {
+    Itinerary.belongsTo(models.Users);
+    Itinerary.hasMany(models.Events);
+    Itinerary.hasMany(models.Events);
+};
   return Itinerary;
 };
