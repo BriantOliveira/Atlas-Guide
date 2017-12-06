8d64defdc5c9a34c0f74113bb3653d23260000e2
Justin Sitter <justin@jaytria.com>
2017-12-06 15:49:27 -0800

    updated stuff

--- routes/signup.js
+++ routes/signup.js
@@ -8,7 +8,6 @@ var jwt = require('jsonwebtoken')
 
  module.exports = function(app){
 
-    process.env.SECRET = "SEECRRet"
 
      //SINGUP
      app.get('/signup', function (req, res) {
@@ -27,22 +26,22 @@ var jwt = require('jsonwebtoken')
 
 
      app.get('/login', function(req, res) {
-         res.redirect('/');
+         res.render('login');
      });
 
 
-     app.get('/login', function(req, res, next) {
-    User.findOne({ username: req.body.username }, "+password", function (err, user) {
-      if (!user) { return res.status(401).send({ message: 'Wrong username or password' }) };
-      user.comparePassword(req.body.password, function (err, isMatch) {
-        if (!isMatch) {
-          return res.status(401).send({ message: 'Wrong Username or password' });
-        }
+     app.post('/login', function(req, res, next) {
+        User.findOne({ username: req.body.username }, "+password", function (err, user) {
+        if (!user) { return res.status(401).send({ message: 'Wrong username or password' }) };
+        user.comparePassword(req.body.password, function (err, isMatch) {
+            if (!isMatch) {
+                return res.status(401).send({ message: 'Wrong Username or password' });
+            }
 
-        var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
-        res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+            var token = jwt.sign({ _id: user._id }, process.env.SECRETKEY, { expiresIn: "60 days" });
+            res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
 
-        res.render('login')
+            res.render('login')
       });
     }).catch(function(err) {
       if(err){
--- server.js
+++ server.js
@@ -42,12 +42,12 @@ let verifyAuthentication = (req, res, next) => {
 /****************************************************
  *  Add Middlewarez
  ***************************************************/
-
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(verifyAuthentication)
 // Set up a static public directory
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
 // Setup handlebars view engine and pass in parameters
 app.engine('hbs', hbs({defaultLayout: 'main', extname: 'hbs'}))
 app.set('view engine', 'hbs')
@@ -59,20 +59,20 @@ require('./routes/signup.js')(app);
 require('./routes/itinerary.js')(app);
 require('./routes/explore.js')(app);
 
-// Add 404 Error page routing
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-//Redirect to 404 page
-app.use(function (err, req, res, next) {
-  if (err.status == 404) {
+// // Add 404 Error page routing
+// app.use(function (req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+// //Redirect to 404 page
+// app.use(function (err, req, res, next) {
+//   if (err.status == 404) {
 
-    //do logging and user-friendly error message display
-    res.redirect('/404.html');
-  }
-});
+//     //do logging and user-friendly error message display
+//     res.redirect('/404.html');
+//   }
+// });
 
 
 // Listen on port
