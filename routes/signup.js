fdaa483ec5d5504679f1d40d3182a953c8f038ac
Briant Oliveira <briantmoliveira@gmail.com>
2017-12-06 15:50:54 -0800

    env removed and changed

--- routes/signup.js
+++ routes/signup.js
@@ -30,24 +30,6 @@ var jwt = require('jsonwebtoken')
      });
 
 
-  //    app.get('/login', function(req, res, next) {
-  //   User.findOne({ username: req.body.username }, "+password", function (err, user) {
-  //     if (!user) { return res.status(401).send({ message: 'Wrong username or password' }) };
-  //     user.comparePassword(req.body.password, function (err, isMatch) {
-  //       if (!isMatch) {
-  //         return res.status(401).send({ message: 'Wrong Username or password' });
-  //       }
-  //
-  //       var token = jwt.sign({ _id: user._id }, process.env.SECRETKEY, { expiresIn: "60 days" });
-  //       res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
-  //
-  //       res.render('login')
-  //     });
-  //   }).catch(function(err) {
-  //     if(err){
-  //         console.log(err)
-  //     }
-  //   })
-  // });
+  
 
  };
