c0d93b1cb915021b153a7938ea0e4f675fdb58fa
Justin Sitter <justin@jaytria.com>
2017-12-06 22:42:40 -0800

    updated something

--- server.js
+++ server.js
@@ -3,19 +3,19 @@
  *      Your Source for excellent travel
  *  v. 1.0.0 Beta
  ******************************************/
-require('dotenv').config()
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const express = require('express')
-const favicon = require('serve-favicon')
-const hbs = require('express-handlebars')
-const jwt = require('jsonwebtoken')
-const path = require('path')
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('express-handlebars');
+const jwt = require('jsonwebtoken');
+const path = require('path');
 
 //Instantiate express
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 /****************************************************
  *  SQL Connection
@@ -44,51 +44,51 @@ let verifyAuthentication = (req, res, next) => {
 
       //Synchronous verification
       try{
-        decodedToken = jwt.verify(token, process.env.SECRETKEY)
-        console.log("***Authenticate***")
-        req.user = decodedToken._id
+        decodedToken = jwt.verify(token, process.env.SECRETKEY);
+        console.log("***Authenticate***");
+        req.user = decodedToken._id;
       }catch(err){
-        console.log(err.message)
-      }
-    }
-    next()
-  }
+        console.log(err.message);
+      };
+    };
+    next();
+  };
 
 
 /****************************************************
  *  Add Middlewarez
  ***************************************************/
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-app.use(cookieParser())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
 // app.use(verifyAuthentication)
 // Set up a static public directory
 
 // Setup handlebars view engine and pass in parameters
-app.engine('hbs', hbs({defaultLayout: 'main', extname: 'hbs'}))
-app.set('view engine', 'hbs')
+app.engine('hbs', hbs({defaultLayout: 'main', extname: 'hbs'}));
+app.set('view engine', 'hbs');
 
 
 // Load Routes
-require('./routes/router.js')(app)
+require('./routes/router.js')(app);
 require('./routes/signup.js')(app);
 require('./routes/itinerary.js')(app);
 require('./routes/explore.js')(app);
 
-// // Add 404 Error page routing
-// app.use(function (req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-// //Redirect to 404 page
-// app.use(function (err, req, res, next) {
-//   if (err.status == 404) {
+// Add 404 Error page routing
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+//Redirect to 404 page
+app.use(function (err, req, res, next) {
+  if (err.status == 404) {
 
-//     //do logging and user-friendly error message display
-//     res.redirect('/404.html');
-//   }
-// });
+    //do logging and user-friendly error message display
+    res.redirect('/404.html');
+  };
+});
 
 
 // Listen on port
