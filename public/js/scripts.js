884396aaf8c3a1da5d1d37a179c57af0c419fac3
Briant Oliveira <briantmoliveira@gmail.com>
2017-12-08 01:58:27 -0800

    dropdown animation

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,6 @@
-console.log("Scripts file loaded")
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
