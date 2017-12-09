a48d5c409e32afbb38ff4c5f8a803e1f7a9df40e
Briant Oliveira <briantmoliveira@gmail.com>
2017-12-08 17:15:34 -0800

    front end almost done on index

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,14 +1,14 @@
-$(function() {
-  $('a[href*=#]').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-  });
-});
+// $(function() {
+//   $('a[href*=#]').on('click', function(e) {
+//     e.preventDefault();
+//     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+//   });
+// });
 
 $(document).ready(function(){
     $(window).scroll(function(){
         if($(window).scrollTop() > $(window).height()){
-            $(".nav-down").css({"background-color":"#FFFFFF"});
+            $(".nav-down").css({"background-color":"#E6C274"});
         }
         else{
             $(".navbar").css({"background-color":"transparent"});
