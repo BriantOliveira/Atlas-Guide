41a99f3700610a0ca555a322273a202eaae47853
Briant Oliveira <briantmoliveira@gmail.com>
2017-12-12 13:47:10 -0800

    added new routes

--- routes/attraction.js
+++ routes/attraction.js
@@ -18,15 +18,15 @@ const models = require('../models');
      });
      //SHOW
      app.get('/tourism/:id', (req, res) => {
-         const vendorId = req.params.id;
-         models.Tour.findById(tourId).then((tour) => {
+         const userId = req.params.id;
+         models.Event.findById(eventId).then((event) => {
              res.send("attraction")
          })
      })
 
      //CREATE
      app.post('/tourism/create', (req, res) => {
-         models.Tour.create(req.body).then((tour) => {
+         models.Event.create(req.body).then((event) => {
              res.status(200);
              res.send("created successfully");
          }).catch((err) => {
@@ -38,20 +38,21 @@ const models = require('../models');
      })
      //UPDATE
      app.put('tourism/:id/edit', (req, res) => {
-         const vendorId = req.body.params;
-         models.Tour.update(tourId).then((tour) => {
-             res.status(200).json({msg: 'successfully updated', tour});
+         const userId = req.body.params;
+         models.Event.update(eventId).then((event) => {
+             res.status(200).json({msg: 'successfully updated', event});
          }).catch((err) =>{
-             if(err)
+             if(err){
              res.status(400)
              console.log(err);
+            }
          })
      });
      //DELETE
      app.delete('/tourism/:id', (req, res) => {
-         const vendorId = re.body.params;
-         models.Tour.destroy(tourId).then((tour) => {
-             res.status(200).json({msg: 'successfully deleted', tour});
+         const userId = req.body.params;
+         models.Event.destroy(eventId).then((event) => {
+             res.status(200).json({msg: 'successfully deleted', event});
             }).catch((err) => {
                 if(err) {
                res.status(400).json({msg: 'ERRO: tour was not deleted'})
